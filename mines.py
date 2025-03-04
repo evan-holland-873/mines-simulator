8450abb5f0f3777567580808798730a46168e5a8
Evan <195604890+evan-holland-873@users.noreply.github.com>
2025-03-03 20:13:26 -0500

    Correct payout rule

--- mines.py
+++ mines.py
@@ -1,24 +1,36 @@
 from random import randint
+from scipy.special import binom
 
 class Board:
-    def __init__(self, width: int, height: int, multiplier = 1.19):
+    def __init__(self, width: int, height: int, multiplier_rule = 0.97):
         self.width = width
         self.height = height
-        self.multiplier = multiplier
+
+        self.multiplier = multiplier_rule
         
         self.tiles = width * height
 
-    def random_tile(self):
+    def random_tile(self) -> tuple:
         tile = (randint(1, self.height), randint(1, self.width))
         return tile
     
-    def random_unique_tile(self, compare: list):
+    def random_unique_tile(self, compare: list) -> tuple:
         tile = self.random_tile()
         while tile in compare:
             tile = self.random_tile()
         return tile
+    
+    def calculate_payout(self, n: int, m: int) -> float:
+        k = self.multiplier
+        tiles_choose_picks = binom(self.tiles, n)
+        safe_choose_picks = binom(self.tiles - m, n)
 
-    def bet(self, mines_count: int, bet_amount: float, tiles: int):
+        pay = k * (tiles_choose_picks / safe_choose_picks)
+
+        return pay
+
+
+    def bet(self, mines_count: int, bet_amount: float, tiles: int, total_balance = 0) -> float:
         mine_positions = []
         revealed_tiles = []
 
@@ -29,10 +41,8 @@ class Board:
             tile_pick = self.random_unique_tile(revealed_tiles)
             
             if tile_pick in mine_positions:
-                return 0
+                return total_balance - bet_amount
             
             revealed_tiles.append(tile_pick)
 
-        payout = bet_amount + self.multiplier ** tiles
-
-        return payout
+        return total_balance + self.calculate_payout(tiles, mines_count) * bet_amount
